d7893221d063ec66e1039e1e403d09867131f869
coachhe <coachhe@tencent.com>
2022-01-17 01:54:48 +0800

    reformat

---
+++ cloud-eureka-server7001/src/main/java/com/coachhe/springcloud/EurekaMain7001.java
@@ -0,0 +1,18 @@
+package com.coachhe.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * EnableEurekaServer表示这为Eureka的服务端
+ * @author CoachHe
+ * @date 2022/1/17 1:26 AM
+ **/
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaMain7001 {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaMain7001.class, args);
+    }
+}
