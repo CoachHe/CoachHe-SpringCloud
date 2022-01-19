ee136e4db47b7aca4f035b9c8d0e9f0855821c62
coachhe <coachhe@tencent.com>
2022-01-19 09:30:31 +0800

    add cloud-eureka-7002

---
+++ cloud-eureka-server7002/src/main/java/com/coachhe/springcloud/EurekaMain7002.java
@@ -0,0 +1,17 @@
+package com.coachhe.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author CoachHe
+ * @date 2022/1/19 12:12 AM
+ **/
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaMain7002 {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaMain7002.class, args);
+    }
+}
